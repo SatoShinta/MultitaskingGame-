53ffa0ac2e292407374416548f5ec8a2da18a5a8
SatoShinta <kanten3005@gmail.com>
2024-09-14 18:20:04 +0900

    タイピングの下地作り完了

---
+++ Assets/Scripts/TypingManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypingManager : MonoBehaviour
+{
+    [SerializeField, Header("–â‘è•¶")] Text qText;
+    [SerializeField, Header("“š‚¦")] Text aText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
