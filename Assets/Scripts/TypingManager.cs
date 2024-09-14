8f681d1a0e78669c573f52ec9c5ff0b3239a1987
SatoShinta <kanten3005@gmail.com>
2024-09-14 20:59:04 +0900

    タイピングゲームの基本の部分はできた！（間違ったときの処理はまだ）

--- Assets/Scripts/TypingManager.cs
+++ Assets/Scripts/TypingManager.cs
@@ -1,37 +1,52 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TypingManager : MonoBehaviour
 {
     [SerializeField, Header("問題文")] Text _qText;
-    //[SerializeField, Header("答え")] Text _aText;
+    [SerializeField, Header("答え")] Text _aText;
 
     [SerializeField] string _answerNumber;
     [SerializeField] string _inputText;
     void Start()
     {
-        _inputText = "";
         QuestionNumber();
     }
 
     void Update()
     {
-        if (Input.anyKeyDown)
+        _aText.text = _inputText;
+
+        //テンキーの数字部分か1～9までの数字キーが押されたとき以下の処理
+        if (Input.anyKeyDown && char.IsDigit(Input.inputString[0]))
         {
-            foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+            /*
+             * ASCIIコードを比較して数値を求めている
+             * 例…3(ASCIIコードだと51) - 0(ASCIIコードだと48) = 3となる
+             * 基本的には０が基準なのでほかの数値から0を引けばその数値が求められる
+             * 覚えておきたい
+            */
+            int digit = (int)Input.inputString[0] - '0';
+            _inputText += digit;
+
+            // 入力文字数が問題の桁数を超えない場合
+            if (_inputText.Length <= _answerNumber.Length)
             {
-                if (Input.GetKeyDown(key) && key >= KeyCode.Alpha0 && key <= KeyCode.Alpha9)
-                {                                       
-                    //入力したKeyCode.Alphaの内部の値から
-                    //KeyCode.Alpha0の数を引いて余計な部分（Alphaの文字）を消して代入している
-                    //5を入力した場合…KeyCode.Alpha5(53) - KeyCode.Alpha0(48) = 5!!!!!
-                    int digit = (int)key - (int)KeyCode.Alpha0;
-                    _inputText += digit;
-                    Debug.Log(_inputText);
+                // 入力された最後の文字と問題の対応する文字を比較
+                int lastIndex = _inputText.Length - 1;
+                if (_inputText[lastIndex] != _answerNumber[lastIndex])
+                {
+                    // 間違っていた場合の処理 
+                    Debug.Log("間違っています");
+
+                    // 入力された最後の文字を削除
+                    _inputText = _inputText.Remove(lastIndex);
+                    _aText.text = _inputText;
                 }
             }
-            if (_inputText == _answerNumber)
+
+            //文字数と内容がすべて一致したら
+            if (_inputText.Length == _answerNumber.Length &&_inputText == _answerNumber)
             {
                 QuestionNumber();
                 _inputText = "";
