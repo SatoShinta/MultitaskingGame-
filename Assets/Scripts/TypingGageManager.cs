a687d6f76dc18d81a313aacf1e6c4b217acef4bc
SatoShinta <kanten3005@gmail.com>
2024-09-16 17:48:16 +0900

    時間制限の演出が完成した

--- Assets/Scripts/TypingGageManager.cs
+++ Assets/Scripts/TypingGageManager.cs
@@ -1,16 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TypingGageManager : MonoBehaviour
 {
+    [SerializeField] GameObject _backGauge;
+    [SerializeField] GameObject _timerGauge;
+
+    [SerializeField, Header("制限時間")] float _timerLimit = 1f; // 1単位のゲージを減らすのにかかる時間 (秒)
+    [SerializeField]Gradient _gradient; //色のグラデーションを変更してくれるやつ
+
+    private float _timer;
+    public bool _gameStart;
+    public bool _timerReset;
+    Image _gaugeImage;
+
     void Start()
     {
-        
+        // _timerGaugeの初期幅を_backGaugeの幅に合わせる
+        _timerGauge.GetComponent<RectTransform>().sizeDelta = _backGauge.GetComponent<RectTransform>().sizeDelta;
+        // 初期の色を設定（白）
+        _gaugeImage = _timerGauge.GetComponent<Image>();
+        _gaugeImage.color = _gradient.Evaluate(0);
     }
 
     void Update()
     {
-        
+        if (_gameStart)
+        {
+            _timer += Time.deltaTime;
+            Debug.Log(_timer);
+
+            // _timerGaugeの幅を計算
+            float gaugeWidth = Mathf.Max(0, _backGauge.GetComponent<RectTransform>().sizeDelta.x - (_timer / _timerLimit * _backGauge.GetComponent<RectTransform>().sizeDelta.x));
+
+            // _timerGaugeの幅を更新
+            _timerGauge.GetComponent<RectTransform>().sizeDelta = new Vector2(gaugeWidth, _timerGauge.GetComponent<RectTransform>().sizeDelta.y);
+
+            // 制限時間に近づくにつれて色が変化していくようにした
+            _gaugeImage.color = _gradient.Evaluate(_timer / _timerLimit);
+            if(_timerReset == true)
+            {
+                _timerReset = false;
+                _timer = 0;
+            }
+        }
     }
 }
